28e2d55db837466eb4540965a9520df3064865f2
Malekbennasser <malekben0001@gmail.com>
2024-02-26 13:58:16 +0100

    create header component , style

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import mealsImage from "../../assets/14-days-clean-eating-meal-plan-1200-lede-601736337d914519bb5bf8eb83540da1.jpg";
+import classes from "./Header.module.css";
+
+export const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Order food</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt=" A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
